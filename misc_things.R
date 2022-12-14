2c4550440f73e5ceee5ebc085d044d1ebe82d86e
nnethercott <natenethercott@gmail.com>
2022-12-14 12:28:03 +0100

    wrote little function to generate tile surface/matrix from coords

--- misc_things.R
+++ misc_things.R
@@ -22,7 +22,8 @@ class(lds.ppp)
 
 #creating `box` for our coords to fit inside based on easting and northing coords
 #this box is a crude estimate for encapsulating hong kong - USED LATER MAYBE?
-dim = as.integer(c(max(lds$NORTHING)-min(lds$NORTHING), max(lds$EASTING)-min(lds$EASTING))/1000)
+rf = 1000
+dim = as.integer(c(max(lds$NORTHING)-min(lds$NORTHING), max(lds$EASTING)-min(lds$EASTING))/rf) + 1
 dim 
 
 ggplot(lds) + geom_sf() #plot data 
@@ -33,4 +34,15 @@ x11()
 plot(dens, main = 'Intensity')
 
 
+#can i make a crude little image for the locations?  -- BAD since we loose resolution (also missing vals??)
+m = matrix(0, nrow=dim[1], ncol=dim[2])
+for(i in 1:dim(lds)[1]){
+  j = as.integer((lds$NORTHING[i] - min(lds$NORTHING))/rf)+1
+  k = as.integer((lds$EASTING[i] - min(lds$EASTING))/rf)+1
+  m[j,k]=m[j,k]+1
+}
+sum(m)
+dim(lds)[1]
 
+x11()
+image(t(m))
